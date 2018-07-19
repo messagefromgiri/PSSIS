47fe090d5f7a242cac17d08267a79126d7bc2d51
sunny-g7 <sundeep.paluru@gmail.com>
2018-07-19 23:07:25 +0530

    Updated Web DSC

--- PD-VM-Deployments-East02/Premium-SIS-WEB-DSC-UE-DJ-v2/DSC/SIS-WEB-DSC.ps1
+++ PD-VM-Deployments-East02/Premium-SIS-WEB-DSC-UE-DJ-v2/DSC/SIS-WEB-DSC.ps1
@@ -274,11 +274,16 @@ Node $nodeName
 			if(Get-Service -Name newrelic-infra -ErrorAction Ignore){return $true}else{$false}
 		}
 		SetScript = {
-            $acctKey = ConvertTo-SecureString -String "rLv9/xj+lecCIHZEqvntmhDqD8SOzOrvXWhcXUAi8pFNjNTwQJLJADw8YzqCwOJNUft5hYDVDdoqmZT7WqkjoA==" -AsPlainText -Force
-            $credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\sispd02sragrssa003", $acctKey
-            New-PSDrive -Name Z -PSProvider FileSystem -Root "\\sispd02sragrssa003.file.core.windows.net\pssis" -Credential $credential -Persist
-			Start-Sleep -Seconds 90
-			#net use Z: \\sispd02sragrssa003.file.core.windows.net\pssis /u:AZURE\sispd02sragrssa003 rLv9/xj+lecCIHZEqvntmhDqD8SOzOrvXWhcXUAi8pFNjNTwQJLJADw8YzqCwOJNUft5hYDVDdoqmZT7WqkjoA==       
+			try{
+                $acctKey = ConvertTo-SecureString -String "rLv9/xj+lecCIHZEqvntmhDqD8SOzOrvXWhcXUAi8pFNjNTwQJLJADw8YzqCwOJNUft5hYDVDdoqmZT7WqkjoA==" -AsPlainText -Force
+                $credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\sispd02sragrssa003", $acctKey
+                New-PSDrive -Name Z -PSProvider FileSystem -Root "\\sispd02sragrssa003.file.core.windows.net\pssis" -Credential $credential -Persist
+                Start-Sleep -Seconds 150
+			}
+			catch{
+				net use Z: \\sispd02sragrssa003.file.core.windows.net\pssis /u:AZURE\sispd02sragrssa003 rLv9/xj+lecCIHZEqvntmhDqD8SOzOrvXWhcXUAi8pFNjNTwQJLJADw8YzqCwOJNUft5hYDVDdoqmZT7WqkjoA==
+				Start-Sleep -Seconds 150
+			}
                 if(!(Test-Path -Path D:\NEWRELIC\newrelic-infra.msi -ErrorAction SilentlyContinue)){			        
 
                         New-Item -Path D:\ -ItemType Directory -Name NEWRELIC -ErrorAction SilentlyContinue
