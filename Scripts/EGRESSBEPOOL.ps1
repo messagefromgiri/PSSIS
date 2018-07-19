fd221a7b62034761708c6613ff0aa93e596d6f8c
sunny-g7 <sundeep.paluru@gmail.com>
2018-07-19 23:06:53 +0530

    Dynamic-Egress-Automated

--- Scripts/EGRESSBEPOOL.ps1
+++ Scripts/EGRESSBEPOOL.ps1
@@ -1,23 +1,23 @@
 ﻿#Subscription ID of the Application subscription with the FE machines
-$subscriptionId = "8adc65ab-342a-48ca-ba24-c0a43042c519"
+$subscriptionId = "8e7f0b90-1e44-4729-a2d3-a85c4fdc3625"
 
 #Resource Group that has the Public Load balancer
-$lbResourceGroup = "TIER-PD-NW-01-T0-RG001"
+$lbResourceGroup = "TIER-PD-NW-02-T0-RG001"
 
 #Name of the load balancer
-$lbname = "SIS-PD-FE-EGRESS-ELB001"
+$lbname = "SIS-PD-FE-02-EGRESS-ELB001"
 
 #backend pool name
 $bepoolname = "SIS-FE-POOL"
 
 #Front End Resource Group  
-$feresourcegroup = 'SIS-PD-ST-00-FE-RG001'
+$feresourcegroup = 'SIS-PD-ST-02-FE-RG001'
 
 #Path to CSV
-$pathtocsv = "C:\Users\bhushanb\OneDrive - Microsoft\PowerSchool\sisnicnames.csv"
+#$pathtocsv = "C:\Users\bhushanb\OneDrive - Microsoft\PowerSchool\sisnicnames.csv"
 
 #Login to Azure
-Login-AzureRmAccount
+#Login-AzureRmAccount
 
 #Select Subscription
 Select-AzureRmSubscription -Subscription $subscriptionId
@@ -29,17 +29,19 @@ $Loadbalancer = Get-AzureRmLoadBalancer -ResourceGroupName $lbResourceGroup -Nam
 $backend = Get-AzureRmLoadBalancerBackendAddressPoolConfig -name $bepoolname -LoadBalancer $Loadbalancer
 
 #Get CSV with NIC names
-$niccsv = $pathtocsv
+#$niccsv = $pathtocsv
 
 #Import CSV
-$nicnames = Import-Csv $niccsv
-
+#$nicnames = Import-Csv $niccsv
+$vmList = Get-AzureRmVM -ResourceGroupName $feresourcegroup | Where-Object {$_.Name -match "APPW"}
+$NicList = $vmList | % { $_.NetworkProfile.NetworkInterfaces | ? {$_.primary -eq $true}}
 #Assign NIC to backend pool.
 #Validate column names from CSV and update as necessary
-foreach ($nicname in $nicnames)
-{
-    $nic = Get-AzureRmNetworkInterface –name $nicname.name -resourcegroupname $feresourcegroup
-    $nic.IpConfigurations[0].LoadBalancerBackendAddressPools=$backend
-    Set-AzureRmNetworkInterface -NetworkInterface $nic
+foreach ($nicname in $NicList) {
+    $nic = Get-AzureRmNetworkInterface –name ($nicname.id).Substring(135) -resourcegroupname $feresourcegroup
+    if (!($nic.IpConfigurations|? {$_.Name -eq "Main-IP"}).LoadBalancerBackendAddressPools[0].ID -match $bepoolname) {
+        Write-Host "Adding ($nicname.id).Substring(135) to $bepoolname"
+        $nic.IpConfigurations[0].LoadBalancerBackendAddressPools = $backend
+        Set-AzureRmNetworkInterface -NetworkInterface $nic
+    }
 }
-
